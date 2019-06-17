9f052e9fe15add4b41011584646b0ae5582bfef7
Park Eun Bum <mpark6233@spc.co.kr>
2019-06-17 17:40:56 +0900

    updated sidebar layout

--- js/main.js
+++ js/main.js
@@ -8,86 +8,109 @@
 
 ( function( $ ) {
 
-	// ----- Site visible after all elements are loaded. Fix object fit for IE
+	// ----- Base & Cross Browsing ----- //
 	$(document).ready(function() {
-	  document.getElementsByTagName("html")[0].style.visibility = "visible";
-		objectFitImages();
+    // Show site after all elements are loaded
+    document.getElementsByTagName("html")[0].style.visibility = "visible";
+    // Execute object fit hack for IE
+    objectFitImages();
 	});
 
-  // ----- Cross browser vh range fix
+  // --- vh hack: height: 100vh; height: calc(var(--vh, 1vh) * 100); -- //
   // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
   let vh = window.innerHeight * 0.01;
   // Then we set the value in the --vh custom property to the root of the document. `${vh}px`
   document.documentElement.style.setProperty('--vh', vh + 'px');
-  // CSS set -> height: 100vh; height: calc(var(--vh, 1vh) * 100);
 
-  // ----- Fixed navigation scripts
-  // --- Fixed navigation header add sticky class on scroll
-	$(document).ready(function() {
-    // When the user scrolls the page, execute myFunction
-    window.onscroll = function() {myFunction()};
-    // Get the header
-    var header = document.getElementById("masthead");
-    // If top header exist, get main header
-    var mainheader = document.getElementById("main-header");
-    
-    // // Get the offset position of the header
-    // var sticky = header.offsetTop;
-    // If top header exist, get the offset position of the main header instead
-    var sticky = mainheader.offsetTop;
-
-    var wpadminbar = $('#wpadminbar').height();
-
-    var headerheight = $('#masthead').height();
-    
-    // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-    function myFunction() {
-      // For width <= 600px, add sticky class after scroll y pos is >= 46px
-      if (window.innerWidth <= 600 && window.pageYOffset > wpadminbar || (window.innerWidth > 600 && window.pageYOffset > sticky)) {
-        // Add sticky class on scroll
-        header.classList.add("sticky");
-        // Add absolute position on scroll
-        $('#masthead').css('position', 'fixed');
-        // Add page content top spacing on scroll
-        $('.site-content').css('margin-top', headerheight);
-      } else {
-        header.classList.remove("sticky");
-        // Add relative on top pos
-        $('#masthead').css('position', 'relative');
-        $('.site-content').css('margin-top', 0); // reset page content top spacing
-      }
-    };
-  });
-
-  // add sidebar class to body when sidebar is active
+  // ----- Underscores Theme Custom ----- //
+  // --- Sidebar --- //
+  // Add class to body when sidebar is active
   $(document).ready(function() {
     if ( $('#secondary').length > 0 ) {
       $('body').addClass('has-sidebar');
     }
-    // add bootstrap container & row to sidebar layout
+  });
+  // --- Fixed Header --- //
+	$(document).ready(function() {
+    // When the user scrolls the page, execute addFixedHeader
+    window.onscroll = function() { addFixedHeader() };
+    // Get the header
+    var masthead = document.getElementById("masthead");
+    // If top header exist, get the main header
+    var mainheader = document.getElementById("main-header");
+  
+    // If top header exist, get the offset position of the main header
+    var disttotop = mainheader.offsetTop;
+
+    // Get the Wordpress admin bar height
+    var wpadminbarheight = $('#wpadminbar').height();
+    // Get the header height
+    var mastheadheight = $('#masthead').height();
+    
+    // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+    function addFixedHeader() {
+      // For width <= 600px, add sticky class after scroll y pos is >= 46px
+      if (window.innerWidth <= 600 && window.pageYOffset > wpadminbarheight || (window.innerWidth > 600 && window.pageYOffset > disttotop)) {
+        // Add sticky class on scroll
+        masthead.classList.add("sticky");
+        // Add fixed position on scroll
+        $('#masthead').css('position', 'fixed');
+        // Add page content top spacing on scroll
+        $('.site-content').css('margin-top', mastheadheight);
+      } else {
+        masthead.classList.remove("sticky");
+        // Add relative position on top pos
+        $('#masthead').css('position', 'relative');
+        // Reset page content top spacing
+        $('.site-content').css('margin-top', 0);
+      }
+    };
+  });
+  // ----- Bootstrap Support ----- //
+  // --- Sidebar --- //
+  $(document).ready(function() {
+    // Add class to elements if sidebar exist
     if ( $('body').hasClass('has-sidebar') ) {
-      $('.content-area').addClass('container');
-      $('.site-main').addClass('row');
+      $('.site-content').wrapInner("<div class='container' />");
+    };
+    // Add class to elements if sidebar does not exist
+    if ( !$('body').hasClass('has-sidebar') ) {
+      $('header.page-header').addClass('container');
+      $('header.entry-header').addClass('container');
     };
   });
 
-  // add hamburgers support for bootstrap mobile menu
+  // --- Archive --- //
+  // Add bootstrap grid to archive posts
+  if ( $('body').hasClass('archive') ) {
+    $('.site-main > article').wrapAll("<div class='row' />");
+    $('article').addClass('col-12 col-sm-6 col-md-4');
+  };
+
+  // --- Gutenberg  --- //
+  $('.wp-block-columns').addClass('row');
+  $('.wp-block-column').addClass('col');
+
+  // --- Stackable - Gutenberg Blocks --- //
+  $(document).ready(function() {
+    if ( $('.ugb-container').hasClass('fullwidth') ) {
+      // Add class for no sidebar and fullwidth
+      $('body:not(.has-sidebar) .fullwidth .ugb-container__content-wrapper').addClass('container-fluid');
+    }
+    else {
+      // Add class for no sidebar and boxed
+      $('body:not(.has-sidebar) .ugb-container').addClass('boxed');
+      $('.boxed .ugb-container__content-wrapper').addClass('container');
+      $('.boxed .ugb-container__content-wrapper').attr('style','margin-right: auto; margin-left: auto');
+    }
+  });
+  
+  // --- Woocommerce --- //
+  $('body.woocommerce .site-content').wrapInner("<div class='container' />");
+
+  // --- Hamburgers Menu --- //
   $('.navbar-toggle').click(function () {
     $('.navbar-toggle').toggleClass('is-active');
   });
-
-	// add bootstrap container to gutenberg wp-block-column class
-	$('header.page-header').addClass('container');
-	$('header.entry-header').addClass('container');
-  $('body:not(.no-sidebar) .site-content').wrapInner("<div class='container' />");
-
-  // append header banner to site content
-  $('.header-banner').insertBefore('.site-content > .container');
-
-
-  // add bootstrap grid to archive posts
-  if ( $('body').hasClass('archive') ) {
-    $('article').addClass('col-12 col-sm-6 col-md-4');
-  };
 
 } )( jQuery );
